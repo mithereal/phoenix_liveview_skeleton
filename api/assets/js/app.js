af9e9a25aff099312127897b4110b6c2b561e3fc
mithereal <mithereal@gmail.com>
2021-03-17 02:52:50 +0000

    .

--- api/assets/js/app.js
+++ api/assets/js/app.js
@@ -19,8 +19,7 @@ import {LiveSocket} from "phoenix_live_view"
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
 
-// Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+
 window.addEventListener("phx:page-loading-start", info => NProgress.start())
 window.addEventListener("phx:page-loading-stop", info => NProgress.done())
 
