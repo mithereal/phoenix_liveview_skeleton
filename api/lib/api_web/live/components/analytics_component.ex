25c8242fa072cd1dedb2035d425f914704173449
mithereal <mithereal@gmail.com>
2021-03-17 22:32:23 +0000

    .

--- api/lib/api_web/live/components/analytics_component.ex
+++ api/lib/api_web/live/components/analytics_component.ex
@@ -15,6 +15,22 @@ defmodule ApiWeb.AnalyticsComponent do
             </div>
 
             <div class="flex flex-wrap">
+                               <div class="w-full md:w-1/2 xl:w-1/3 p-6">
+                    <!--Metric Card-->
+                    <div class="bg-gradient-to-b from-blue-200 to-blue-100 border-b-4 border-blue-500 rounded-lg shadow-xl p-5">
+                        <div class="flex flex-row items-center">
+                            <div class="flex-shrink pr-4">
+                                <div class="rounded-full p-5 bg-blue-600"><i class="fas fa-server fa-2x fa-inverse"></i></div>
+                            </div>
+                            <div class="flex-1 text-right md:text-center">
+                                <h5 class="font-bold uppercase text-gray-600">Server Uptime</h5>
+                                <h3 class="font-bold text-3xl"><%= @uptime  %> Minutes</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <!--/Metric Card-->
+                </div>
+
                 <div class="w-full md:w-1/2 xl:w-1/3 p-6">
                     <!--Metric Card-->
                     <div class="bg-gradient-to-b from-pink-200 to-pink-100 border-b-4 border-pink-500 rounded-lg shadow-xl p-5">
@@ -30,12 +46,12 @@ defmodule ApiWeb.AnalyticsComponent do
                     </div>
                     <!--/Metric Card-->
                 </div>
-                <div class="w-full md:w-1/2 xl:w-1/3 p-4">
+                <div class="w-full md:w-1/2 xl:w-1/3 p-6">
                     <!--Metric Card-->
                     <div class="bg-gradient-to-b from-green-200 to-green-100 border-b-4 border-yellow-600 rounded-lg shadow-xl p-5">
                         <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-2">
-                                <div class="rounded-full p-3 bg-yellow-600"><i class="fas fa-user-plus fa-2x fa-inverse"></i></div>
+                            <div class="flex-shrink pr-4">
+                                <div class="rounded-full p-5 bg-yellow-600"><i class="fas fa-user-plus fa-2x fa-inverse"></i></div>
                             </div>
                             <div class="flex-1 text-right md:text-center">
                                 <h5 class="font-bold uppercase text-gray-600">Active Users</h5>
@@ -45,27 +61,12 @@ defmodule ApiWeb.AnalyticsComponent do
                     </div>
                     <!--/Metric Card-->
                 </div>
-                <div class="w-full md:w-1/2 xl:w-1/3 p-4">
-                    <!--Metric Card-->
-                    <div class="bg-gradient-to-b from-blue-200 to-blue-100 border-b-4 border-blue-500 rounded-lg shadow-xl p-5">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-2">
-                                <div class="rounded-full p-3 bg-blue-600"><i class="fas fa-server fa-2x fa-inverse"></i></div>
-                            </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <h5 class="font-bold uppercase text-gray-600">Server Uptime</h5>
-                                <h3 class="font-bold text-3xl"><%= @uptime  %> Minutes</h3>
-                            </div>
-                        </div>
-                    </div>
-                    <!--/Metric Card-->
-                </div>
-                <div class="w-full md:w-1/2 xl:w-1/3 p-4">
+                 <div class="w-full md:w-1/2 xl:w-1/3 p-6">
                     <!--Metric Card-->
                     <div class="bg-gradient-to-b from-red-200 to-red-100 border-b-4 border-red-500 rounded-lg shadow-xl p-5">
                         <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-2">
-                                <div class="rounded-full p-3 bg-red-600"><i class="fas fa-inbox fa-2x fa-inverse"></i></div>
+                            <div class="flex-shrink pr-4">
+                                <div class="rounded-full p-5 bg-red-600"><i class="fas fa-inbox fa-2x fa-inverse"></i></div>
                             </div>
                             <div class="flex-1 text-right md:text-center">
                                 <h5 class="font-bold uppercase text-gray-600">Flames</h5>
@@ -75,6 +76,37 @@ defmodule ApiWeb.AnalyticsComponent do
                     </div>
                     <!--/Metric Card-->
                 </div>
+                <div class="w-full md:w-1/2 xl:w-1/3 p-6">
+                    <!--Metric Card-->
+                    <div class="bg-gradient-to-b from-green-200 to-green-100 border-b-4 border-green-600 rounded-lg shadow-xl p-5">
+                        <div class="flex flex-row items-center">
+                            <div class="flex-shrink pr-4">
+                                <div class="rounded-full p-5 bg-green-600"><i class="fa fa-wallet fa-2x fa-inverse"></i></div>
+                            </div>
+                            <div class="flex-1 text-right md:text-center">
+                                <h5 class="font-bold uppercase text-gray-600">Placeholder</h5>
+                                <h3 class="font-bold text-3xl"> <span class="text-green-500"><i class="fas fa-caret-up"></i></span></h3>
+                            </div>
+                        </div>
+                    </div>
+                    <!--/Metric Card-->
+                </div>
+                <div class="w-full md:w-1/2 xl:w-1/3 p-6">
+                    <!--Metric Card-->
+                    <div class="bg-gradient-to-b from-indigo-200 to-indigo-100 border-b-4 border-indigo-500 rounded-lg shadow-xl p-5">
+                        <div class="flex flex-row items-center">
+                            <div class="flex-shrink pr-4">
+                                <div class="rounded-full p-5 bg-indigo-600"><i class="fas fa-tasks fa-2x fa-inverse"></i></div>
+                            </div>
+                            <div class="flex-1 text-right md:text-center">
+                                <h5 class="font-bold uppercase text-gray-600">Placeholder</h5>
+                                <h3 class="font-bold text-3xl"></h3>
+                            </div>
+                        </div>
+                    </div>
+                    <!--/Metric Card-->
+                </div>
+
             </div>
 
 
