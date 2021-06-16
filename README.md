97748388e80fe4a47ba2a02e22c5e998d31c4c1e
mithereal <mithereal@gmail.com>
2021-06-16 03:09:10 +0000

    update readme

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # Api
 
-This is a liveview with auth demo mostly lifted from https://github.com/leanpanda-com/liveview-authorisation but reorganized to be a generalized starter skeleton
+This is a liveview with auth demo useful for a starter application
 
+[![Build Status](https://travis-ci.com/mithereal/phoenix_liveview_skeleton.svg?branch=master)](https://travis-ci.com/mithereal/phoenix_liveview_skeleton)
+
+[![Inline docs](http://inch-ci.org/github/mithereal/phoenix_liveview_skeleton.svg)](http://inch-ci.org/github/mithereal/phoenix_liveview_skeleton)
+
+To setup your server:
+
+  * Create a user called platform
+  * Install nginx
+  * Install certbot (lets encrypt)
+  * a basic nginx configuration with websockets and environment vars is located in the server directory.
+  
+  
 To start your Phoenix server:
 
   * Install dependencies with `mix deps.get`
@@ -16,6 +28,7 @@ Ready to run in production? Please [check our deployment guides](https://hexdocs
 ## Learn more
 
   * Official website: https://www.phoenixframework.org/
+  * liveview-authorisation: https://github.com/leanpanda-com/liveview-authorisation 
   * Guides: https://hexdocs.pm/phoenix/overview.html
   * Docs: https://hexdocs.pm/phoenix
   * Forum: https://elixirforum.com/c/phoenix-forum
--- lib/api/channel.ex
+++ lib/api/channel.ex
@@ -19,41 +19,41 @@ defmodule Api.Channel do
           end
 
         Logger.info("subscribing to #{channel}")
+
         PubSub.subscribe(Api.PubSub, channel, link: true)
       end
 
-      def notify(params \\ {:error, "Data is empty"}) do
+      def broadcast(params \\ {:error, "Data is empty"}) do
         params
-        |> notify_subscribers([:data, :updated])
+        |> broadcast_subscribers([:data, :updated])
       end
 
-      def notify(params \\ {:error, "Data is empty"}, topic, room) do
-        Logger.info("notification ")
+      def broadcast(params \\ {:error, "Data is empty"}, topic, room) do
 
         params
-        |> notify_subscribers([:data, :updated], topic, room)
+        |> broadcast_subscribers([:data, :updated], topic, room)
       end
 
-      defp notify_subscribers({:ok, result}, event) do
-        Logger.info("notification event  #{event}")
+      defp broadcast_subscribers({:ok, result}, event) do
+
         PubSub.broadcast(Api.PubSub, @topic, {__MODULE__, event, result})
         {:ok, result}
       end
 
-      defp notify_subscribers({:ok, result}, event, topic) do
+      defp broadcast_subscribers({:ok, result}, event, topic) do
         channel =
           case topic do
             nil -> @topic
             _ -> topic
           end
 
-        Logger.info("notification sent to channel #{channel}")
+        Logger.info("broadcast sent to channel #{channel}")
 
         PubSub.broadcast(Api.PubSub, channel, {__MODULE__, event, result})
         {:ok, result}
       end
 
-      defp notify_subscribers({:ok, result}, event, topic, room) do
+      defp broadcast_subscribers({:ok, result}, event, topic, room) do
         topic =
           case topic == nil do
             true -> @topic
@@ -66,13 +66,13 @@ defmodule Api.Channel do
             true -> topic
           end
 
-        Logger.info("notification event sent to channel #{channel}")
+        Logger.info("broadcast event sent to channel #{channel}")
 
         PubSub.broadcast(Api.PubSub, channel, {__MODULE__, event, result})
         {:ok, result}
       end
 
-      defp notify_subscribers(_, {:error, reason}, _event), do: {:error, reason}
+      defp broadcast_subscribers(_, {:error, reason}, _event), do: {:error, reason}
     end
   end
 end
