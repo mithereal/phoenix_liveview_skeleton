dc9bea8f8d4ee3e3c27a9d3826cec2dfa4c39898
mithereal <mithereal@gmail.com>
2022-08-14 19:02:19 -0700

    .

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,34 +1,33 @@
-const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin');
 
 module.exports = {
-  purge: [
-    '../lib/**/*.ex',
-    '../lib/**/*.leex',
-    '../lib/**/*.eex',
-    './js/**/*.js'
-  ],
-  darkMode: 'class',
-  theme: {
-    colors: {
-      gray: colors.coolGray,
-      blue: colors.lightBlue,
-      red: colors.rose,
-      pink: colors.fuchsia
+    content: [
+        './*.html',
+        './pages/**/*.html'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "primary": "#1f4b8e",
+                "primary-dark": "#102a52",
+                "secondary": "#182430",
+                "secondary-dark": "#060C11",
+            }
+        },
     },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif']
-    },
-    extend: {
-      spacing: {
-        '128': '32rem',
-        '144': '36rem'
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      }
-    }
-  },
-  variants: {},
-  plugins: []
-};
+    plugins: [
+        require('@tailwindcss/forms'),
+        plugin(function({ addUtilities, theme }) {
+            const newUtilities = {
+                '.custom-scrollbar': {
+                    '.custom-scrollbar::-webkit-scrollbar': { width: '6px' },
+                    '.custom-scrollbar::-webkit-scrollbar-track': { background: theme('bg-secondary')},
+                    '.custom-scrollbar::-webkit-scrollbar-thumb': { background: '#888' },
+                    '.custom-scrollbar::-webkit-scrollbar-thumb:hover': {background: '#555'},
+                }
+            }
+
+            addUtilities(newUtilities, ['responsive', 'hover'])
+        })
+    ],
+}
